95cd39a1f734b7aa9668883653b758f1b5b20d1a
hio20006 <115939543+hio20006@users.noreply.github.com>
2023-01-05 15:43:03 +0800

    Function_2

--- two-tier-application/M11105506_two_tier/M11105506_two_tier/Form1.Designer.cs
+++ two-tier-application/M11105506_two_tier/M11105506_two_tier/Form1.Designer.cs
@@ -29,24 +29,63 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label listLabel;
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.dataTable1DataGridView = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataTable1BindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.function_1 = new M11105506_two_tier.Function_1();
-            this.dataTable1TableAdapter = new M11105506_two_tier.Function_1TableAdapters.DataTable1TableAdapter();
-            this.tableAdapterManager = new M11105506_two_tier.Function_1TableAdapters.TableAdapterManager();
+            this.dataTable1DataGridView = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataTable1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.function_1 = new M11105506_two_tier.Function_1();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.system_info1DataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.listsysteminfo1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.listBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.function_2 = new M11105506_two_tier.Function_2();
+            this.listComboBox = new System.Windows.Forms.ComboBox();
+            this.systeminfo1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.systeminfo1BindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.system_infoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.listBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.listBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataTable1TableAdapter = new M11105506_two_tier.Function_1TableAdapters.DataTable1TableAdapter();
+            this.tableAdapterManager = new M11105506_two_tier.Function_1TableAdapters.TableAdapterManager();
+            this.listTableAdapter = new M11105506_two_tier.Function_2TableAdapters.ListTableAdapter();
+            this.tableAdapterManager1 = new M11105506_two_tier.Function_2TableAdapters.TableAdapterManager();
+            this.system_info1TableAdapter = new M11105506_two_tier.Function_2TableAdapters.system_info1TableAdapter();
+            listLabel = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataTable1DataGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataTable1BindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.function_1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.system_info1DataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listsysteminfo1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBindingSource2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.function_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.systeminfo1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.systeminfo1BindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.system_infoBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBindingSource1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // listLabel
+            // 
+            listLabel.AutoSize = true;
+            listLabel.Font = new System.Drawing.Font("標楷體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            listLabel.Location = new System.Drawing.Point(8, 76);
+            listLabel.Name = "listLabel";
+            listLabel.Size = new System.Drawing.Size(65, 21);
+            listLabel.TabIndex = 2;
+            listLabel.Text = "list:";
             // 
             // tabControl1
             // 
@@ -73,15 +112,19 @@
             this.tabPage1.Text = "Fun_1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // label1
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(792, 424);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(8, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(733, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Function_1：List ordered average of Rmax in different Segments and in different li" +
+    "sts";
             // 
             // dataTable1DataGridView
             // 
@@ -98,38 +141,6 @@
             this.dataTable1DataGridView.RowTemplate.Height = 24;
             this.dataTable1DataGridView.Size = new System.Drawing.Size(786, 357);
             this.dataTable1DataGridView.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(8, 18);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(733, 23);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Function_1：List ordered average of Rmax in different Segments and in different li" +
-    "sts";
-            // 
-            // dataTable1BindingSource
-            // 
-            this.dataTable1BindingSource.DataMember = "DataTable1";
-            this.dataTable1BindingSource.DataSource = this.function_1;
-            // 
-            // function_1
-            // 
-            this.function_1.DataSetName = "Function_1";
-            this.function_1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // dataTable1TableAdapter
-            // 
-            this.dataTable1TableAdapter.ClearBeforeFill = true;
-            // 
-            // tableAdapterManager
-            // 
-            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
-            this.tableAdapterManager.Connection = null;
-            this.tableAdapterManager.system_infoTableAdapter = null;
-            this.tableAdapterManager.UpdateOrder = M11105506_two_tier.Function_1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
             // 
             // dataGridViewTextBoxColumn1
             // 
@@ -151,6 +162,157 @@
             this.dataGridViewTextBoxColumn3.ReadOnly = true;
             this.dataGridViewTextBoxColumn3.Width = 300;
             // 
+            // dataTable1BindingSource
+            // 
+            this.dataTable1BindingSource.DataMember = "DataTable1";
+            this.dataTable1BindingSource.DataSource = this.function_1;
+            // 
+            // function_1
+            // 
+            this.function_1.DataSetName = "Function_1";
+            this.function_1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.AutoScroll = true;
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.system_info1DataGridView);
+            this.tabPage2.Controls.Add(listLabel);
+            this.tabPage2.Controls.Add(this.listComboBox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Fun_2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(730, 46);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Function2：Select a list, and show the number of supercomputers in descending orde" +
+    "r \r\nused in each segment.";
+            // 
+            // system_info1DataGridView
+            // 
+            this.system_info1DataGridView.AutoGenerateColumns = false;
+            this.system_info1DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.system_info1DataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6});
+            this.system_info1DataGridView.DataSource = this.listsysteminfo1BindingSource;
+            this.system_info1DataGridView.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.system_info1DataGridView.Location = new System.Drawing.Point(3, 102);
+            this.system_info1DataGridView.Name = "system_info1DataGridView";
+            this.system_info1DataGridView.RowTemplate.Height = 24;
+            this.system_info1DataGridView.Size = new System.Drawing.Size(786, 319);
+            this.system_info1DataGridView.TabIndex = 3;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "list";
+            this.dataGridViewTextBoxColumn4.HeaderText = "list";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "segment";
+            this.dataGridViewTextBoxColumn5.HeaderText = "segment";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "computer_num";
+            this.dataGridViewTextBoxColumn6.HeaderText = "computer_num";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.ReadOnly = true;
+            // 
+            // listsysteminfo1BindingSource
+            // 
+            this.listsysteminfo1BindingSource.DataMember = "List_system_info1";
+            this.listsysteminfo1BindingSource.DataSource = this.listBindingSource2;
+            // 
+            // listBindingSource2
+            // 
+            this.listBindingSource2.DataMember = "List";
+            this.listBindingSource2.DataSource = this.function_2;
+            // 
+            // function_2
+            // 
+            this.function_2.DataSetName = "Function_2";
+            this.function_2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // listComboBox
+            // 
+            this.listComboBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.systeminfo1BindingSource, "list", true));
+            this.listComboBox.DataSource = this.listBindingSource2;
+            this.listComboBox.DisplayMember = "list";
+            this.listComboBox.FormattingEnabled = true;
+            this.listComboBox.Location = new System.Drawing.Point(79, 76);
+            this.listComboBox.Name = "listComboBox";
+            this.listComboBox.Size = new System.Drawing.Size(121, 20);
+            this.listComboBox.TabIndex = 3;
+            this.listComboBox.ValueMember = "list";
+            // 
+            // systeminfo1BindingSource
+            // 
+            this.systeminfo1BindingSource.DataMember = "system_info1";
+            this.systeminfo1BindingSource.DataSource = this.function_2;
+            // 
+            // systeminfo1BindingSource1
+            // 
+            this.systeminfo1BindingSource1.DataMember = "system_info1";
+            this.systeminfo1BindingSource1.DataSource = this.function_2;
+            // 
+            // system_infoBindingSource
+            // 
+            this.system_infoBindingSource.DataMember = "system_info";
+            this.system_infoBindingSource.DataSource = this.function_2;
+            // 
+            // listBindingSource
+            // 
+            this.listBindingSource.DataMember = "List";
+            this.listBindingSource.DataSource = this.function_2;
+            // 
+            // listBindingSource1
+            // 
+            this.listBindingSource1.DataMember = "List";
+            this.listBindingSource1.DataSource = this.function_2;
+            // 
+            // dataTable1TableAdapter
+            // 
+            this.dataTable1TableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.Connection = null;
+            this.tableAdapterManager.system_infoTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = M11105506_two_tier.Function_1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // listTableAdapter
+            // 
+            this.listTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager1
+            // 
+            this.tableAdapterManager1.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager1.Connection = null;
+            this.tableAdapterManager1.UpdateOrder = M11105506_two_tier.Function_2TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // system_info1TableAdapter
+            // 
+            this.system_info1TableAdapter.ClearBeforeFill = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -166,6 +328,16 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataTable1DataGridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataTable1BindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.function_1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.system_info1DataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listsysteminfo1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBindingSource2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.function_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.systeminfo1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.systeminfo1BindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBindingSource1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -184,6 +356,23 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private Function_2 function_2;
+        private System.Windows.Forms.BindingSource listBindingSource;
+        private Function_2TableAdapters.ListTableAdapter listTableAdapter;
+        private Function_2TableAdapters.TableAdapterManager tableAdapterManager1;
+        private System.Windows.Forms.BindingSource listBindingSource1;
+        private System.Windows.Forms.BindingSource system_infoBindingSource;
+        private System.Windows.Forms.BindingSource systeminfo1BindingSource;
+        private Function_2TableAdapters.system_info1TableAdapter system_info1TableAdapter;
+        private System.Windows.Forms.ComboBox listComboBox;
+        private System.Windows.Forms.BindingSource systeminfo1BindingSource1;
+        private System.Windows.Forms.BindingSource listBindingSource2;
+        private System.Windows.Forms.DataGridView system_info1DataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.BindingSource listsysteminfo1BindingSource;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- two-tier-application/M11105506_two_tier/M11105506_two_tier/Form1.cs
+++ two-tier-application/M11105506_two_tier/M11105506_two_tier/Form1.cs
@@ -26,6 +26,10 @@ namespace M11105506_two_tier
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // TODO: 這行程式碼會將資料載入 'function_2.system_info1' 資料表。您可以視需要進行移動或移除。
+            this.system_info1TableAdapter.Fill(this.function_2.system_info1);
+            // TODO: 這行程式碼會將資料載入 'function_2.List' 資料表。您可以視需要進行移動或移除。
+            this.listTableAdapter.Fill(this.function_2.List);
             // TODO: 這行程式碼會將資料載入 'function_1.DataTable1' 資料表。您可以視需要進行移動或移除。
             this.dataTable1TableAdapter.Fill(this.function_1.DataTable1);
 
@@ -49,6 +53,19 @@ namespace M11105506_two_tier
             try
             {
 
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void fillToolStripButton_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                
             }
             catch (System.Exception ex)
             {
